734251a5d1565621799a817d543470500f14e182
Candela Echazu <gcandela894@gmail.com>
2025-03-21 13:31:40 -0300

    eliminamos process

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: process.env["API_URL"] || 'https://matchmaking-gz5o.onrender.com/ranking'
+  apiUrl: 'https://matchmaking-gz5o.onrender.com/ranking'
 };
